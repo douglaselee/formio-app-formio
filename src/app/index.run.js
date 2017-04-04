3f0b213a357532ced79bcf5aae3caff18a9e8840
douglaselee <douglaselee@comcast.net>
2017-04-04 14:31:56 -0400

    Added schema export

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -4,10 +4,14 @@
     .module('formioApp')
     .run([
       '$rootScope',
+      '$timeout',
+      'Formio',
       'AppConfig',
       'FormioAuth',
       function(
         $rootScope,
+        $timeout,
+        Formio,
         AppConfig,
         FormioAuth
       ) {
@@ -18,6 +22,20 @@
         angular.forEach(AppConfig.forms, function(url, form) {
           $rootScope[form] = url;
         });
+
+        $rootScope.export = function($event) {
+          if (!$rootScope.target) {
+            $rootScope.target = $event.currentTarget;
+            Formio.makeStaticRequest(Formio.getApiUrl() + '/export').then(function (result) {
+              $rootScope.template = JSON.stringify(result);
+              $timeout(function() {
+                $rootScope.target.click();
+                $rootScope.target = null;
+              });
+            });
+            $event.preventDefault();
+          }
+        };
       }
     ]);
 })();
