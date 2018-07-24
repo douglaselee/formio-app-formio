df710a54b844a243f147925459826963226766f6
douglaselee <douglaselee@comcast.net>
2018-07-24 14:15:16 -0400

    Reset password stuff

--- src/config.js
+++ src/config.js
@@ -9,6 +9,13 @@ location.search.substr(1).split("&").forEach(function(item) {
   query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
 });
 
+if (query['x-jwt-token']) {
+  localStorage.setItem('formioToken', query['x-jwt-token']);
+  localStorage.removeItem('formioAppUser');
+  localStorage.removeItem('formioUser');
+  window.history.pushState("", "", location.pathname + location.hash);
+}
+
 var appUrl = query.appUrl || APP_URL;
 var apiUrl = query.apiUrl || API_URL;
 
@@ -17,6 +24,8 @@ angular.module('formioApp').constant('AppConfig', {
   apiUrl: apiUrl,
   forms: {
     userForm: appUrl + '/user',
-    userLoginForm: appUrl + '/user/login'
+    userLoginForm: appUrl + '/user/login',
+    sendResetPassword: appUrl + '/sendreset',
+    resetPassForm: appUrl + '/resetpassword'
   }
 });
